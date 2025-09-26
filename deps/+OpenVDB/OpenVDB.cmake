876a26f63e5306b85e23e32eb2b8b081d400b169
Nima0908 <marius.webert@web.de>
2025-09-26 16:32:07 +0200

    OpenVDB fix

--- deps/+OpenVDB/OpenVDB.cmake
+++ deps/+OpenVDB/OpenVDB.cmake
@@ -29,4 +29,7 @@ add_cmake_project(OpenVDB
         -DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
 )
 
+set(Boost_NO_BOOST_CMAKE ON)
+find_package(Boost 1.89 REQUIRED system)
+
 set(DEP_OpenVDB_DEPENDS TBB Blosc OpenEXR Boost)
